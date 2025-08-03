085b20273aefe290448795216532dd0f2d91668c
kevin Viloria <kevindrax971@gmail.com>
2025-08-03 16:33:15 -0500

    Primer commit

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_iYpje0F17fF-O26jk-d9ImAEc2fCP_A",
+  authDomain: "nitroedu-bbe53.firebaseapp.com",
+  databaseURL: "https://nitroedu-bbe53-default-rtdb.firebaseio.com",
+  projectId: "nitroedu-bbe53",
+  storageBucket: "nitroedu-bbe53.firebasestorage.app",
+  messagingSenderId: "758285076680",
+  appId: "1:758285076680:web:e99e0cfe93748f1daebdfd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
